ecc757201b52766d0b858889ea03137c343665c8
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-05-09 17:13:24 -0300

    including React Router in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
     <div className="App">
-      Home
+
+        <Header />
+        <About />
+        <Contacts />
+
     </div>
   );
 }
